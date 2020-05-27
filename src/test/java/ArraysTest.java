af3eade905331afd7490fe093a9383adeb1fadf7
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-05-27 16:09:25 +0530

    performance test

--- src/test/java/ArraysTest.java
+++ src/test/java/ArraysTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.Timeout;
+//import org.junit.jupiter.a
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,5 +13,16 @@ class ArraysTest {
     int[] expected={1,3,5,12};
     Arrays.sort(numbers);
     assertArrayEquals(expected,numbers);
+}
+@Test
+@Timeout(value = 100, unit = TimeUnit.MILLISECONDS)
+    public void testSort_performance()
+{
+    int[] array ={12,3,4};
+    for(int i=0;i<1000000;i++)
+    {
+        array[0]=i;
+        Arrays.sort(array);
+    }
 }
 }
